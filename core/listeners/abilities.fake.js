6a17d0b05f8ea8ff733a28132adf59b94888af66
R-Andrei <faragau.rares@gmail.com>
2019-06-29 12:32:29 +0300

    added listener for me

---
+++ core/listeners/abilities.fake.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'ready.main',
+    description: 'Listener for ready login event.',
+    body: (instance) => {
+        return () => {
+            instance.client.on('message', msg => {
+                if (msg.author.tag === 'Fake#1000' && msg.channel.id === '588668921075335178') {
+                    if (msg.content === '!!terminate') {}
+                    if (msg.content.charAt(0) === '!') {
+                        let split = msg.content.split(' ');
+    
+    
+                    } else if (fake_listener === 'on') {
+                        (msg.content.charAt(0) === '\\') ? send_fake_message(client, msg.content.substring(1)): send_fake_message(client, msg.content);
+                    }
+                }
+            });
+        }
+    }
+}
